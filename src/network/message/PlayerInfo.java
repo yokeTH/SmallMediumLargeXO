7d8798f4f181d6494bba170af496ba27982f35e4
yokeTH <sern.dev@gmail.com>
2023-12-11 13:40:12 +0700

    snapshot

--- src/network/message/PlayerInfo.java
+++ src/network/message/PlayerInfo.java
@@ -1,22 +1,56 @@
 package network.message;
 
+import logic.chess.BaseChess;
 import logic.entity.Player;
 import logic.game.TeamColor;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class PlayerInfo implements Serializable {
     private TeamColor teamColor;
     private String name;
 
-    public PlayerInfo(TeamColor teamColor, String name) {
+    private ArrayList<Chess> inventory;
+
+    public PlayerInfo(TeamColor teamColor, String name, ArrayList<BaseChess> inventory) {
         this.teamColor = teamColor;
         this.name = name;
+//        this.inventory = inventory;
     }
 
     public PlayerInfo(Player player) {
         this.teamColor = player.getTeamColor();
         this.name = player.getName();
+        this.inventory = toChessMessage(player.getBaseChessArrayList());
+    }
+
+    private ArrayList<Chess> toChessMessage(ArrayList<BaseChess> inventory){
+        ArrayList<Chess> chess = new ArrayList<>();
+        for (int i = 0; i < inventory.size(); i++) {
+            switch (inventory.get(i).getSize()){
+                case 3:
+                    chess.add(Chess.Large);
+                    break;
+                case 2:
+                    chess.add(Chess.Medium);
+                    break;
+                case 1:
+                    chess.add(Chess.Small);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return chess;
+    }
+
+    public ArrayList<Chess> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Chess> inventory) {
+        this.inventory = inventory;
     }
 
     public TeamColor getTeamColor() {
--- src/network/message/RoomInfo.java
+++ src/network/message/RoomInfo.java
@@ -1,18 +1,47 @@
 package network.message;
 
-import network.server.Room;
+import logic.entity.Board;
+import logic.game.GameLogic;
 
 public class RoomInfo extends MessageObject{
     private PlayerInfo playerInfo1;
     private PlayerInfo playerInfo2;
 
     private PlayerInfo currentPlayer;
+    private BoardInfo boardInfo;
 
 
-    public RoomInfo(Room room){
+    public BoardInfo getBoardInfo() {
+        return boardInfo;
+    }
+
+    public void setBoardInfo(Board board) {
+        this.boardInfo = new BoardInfo(board);
+    }
+
+    public RoomInfo(GameLogic gameLogic){
         super(MessageType.ROOM_INFO);
-        this.playerInfo1 = new PlayerInfo(room.getPlayer1());
-        this.playerInfo2 = new PlayerInfo(room.getPlayer2());
+        this.playerInfo1 = new PlayerInfo(gameLogic.getPlayer1());
+        this.playerInfo2 = new PlayerInfo(gameLogic.getPlayer2());
+        this.currentPlayer = new PlayerInfo(gameLogic.getCurrentPlayer());
+        this.boardInfo = new BoardInfo(gameLogic.getBoard());
+    }
+
+    public PlayerInfo getPlayerInfo1() {
+        return playerInfo1;
+    }
+
+
+    public void setPlayerInfo1(PlayerInfo playerInfo1) {
+        this.playerInfo1 = playerInfo1;
+    }
+
+    public PlayerInfo getPlayerInfo2() {
+        return playerInfo2;
+    }
+
+    public void setPlayerInfo2(PlayerInfo playerInfo2) {
+        this.playerInfo2 = playerInfo2;
     }
 
     public PlayerInfo getCurrentPlayer() {
